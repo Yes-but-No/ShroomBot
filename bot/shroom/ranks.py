6539bcaa39dc2e1d88c3f5b9fa8cab1144456c90
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-04-05 22:50:22 +0800

    Update ranks.py

--- bot/shroom/ranks.py
+++ bot/shroom/ranks.py
@@ -11,21 +11,23 @@ from dataclasses import dataclass
 class Rank:
   name: str
   requirement: int
+  enum: int
 
 RANKS = (
-  Rank("Shroom Forager", 0),
-  Rank("Fungi Farmer", 50),
-  Rank("Truffle Hunter", 100),
-  Rank("Mycology Enthusiast", 250),
-  Rank("Fungi Apprentice", 500),
-  Rank("Truffle Collector", 1000),
-  Rank("Shroom Maestro", 2500),
-  Rank("Truffle Connoisseur", 5000),
-  Rank("Fungi Master", 10000),
-  Rank("Mycologist", 25000),
-  Rank("Shroom Baron", 50000),
-  Rank("Truffle King/Queen", 100000),
-  Rank("Fungi Overlord", 250000),
-  Rank("Mycology Mogul", 500000),
-  Rank("Shroom Deity", 1000000)
+  #   |     Rank Name     | Required | Enum |
+  Rank("Shroom Forager"     , 0      , 0),
+  Rank("Fungi Farmer"       , 50     , 1),
+  Rank("Truffle Hunter"     , 100    , 2),
+  Rank("Mycology Enthusiast", 250    , 3),
+  Rank("Fungi Apprentice"   , 500    , 4),
+  Rank("Truffle Collector"  , 1000   , 5),
+  Rank("Shroom Maestro"     , 2500   , 6),
+  Rank("Truffle Connoisseur", 5000   , 7),
+  Rank("Fungi Master"       , 10000  , 8),
+  Rank("Mycologist"         , 25000  , 9),
+  Rank("Shroom Baron"       , 50000  , 10),
+  Rank("Truffle King/Queen" , 100000 , 11),
+  Rank("Fungi Overlord"     , 250000 , 12),
+  Rank("Mycology Mogul"     , 500000 , 13),
+  Rank("Shroom Deity"       , 1000000, 14)
 )
