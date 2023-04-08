a185894d2055888974bc3a7eb5e64f76c29c430e
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-04-08 18:53:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from discord.ext.commands import when_mentioned_or
 from bot import ShroomBot
 
 DATABASE_URL = os.getenv("MONGO_URL")
+DEV_SERVER = discord.Object(id=os.getenv("DEV_SERVER_ID")) # type: ignore
 
 if DATABASE_URL is None:
   url = "localhost"
@@ -15,6 +16,7 @@ else:
   url = DATABASE_URL
 
 bot = ShroomBot(
+  dev_server=DEV_SERVER,
   mongo_url=url,
   command_prefix=when_mentioned_or("$"),
   help_command=None,
