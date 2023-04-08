c2bc5d73b41f7f0d05bd660b7fd74c72ab01e819
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-04-08 17:41:37 +0800

    Create constants.py

---
+++ bot/constants.py
@@ -0,0 +1,16 @@
+__version__ = "1.0.0"
+
+WHATS_NEW = """
+**v1.0.0**
+ - Added changelog
+"""
+
+CHANGE_LOG = f"""
+{WHATS_NEW}
+"""
+
+EXTENSIONS = (
+  "bot.cog.debug",
+  "bot.cog.farm",
+  "bot.cog.misc"
+)
